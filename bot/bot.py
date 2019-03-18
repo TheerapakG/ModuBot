ea56d630ed76bcdc40047b7c0f660b4d068bb012
TheerapakG <theerapakg@gmail.com>
2019-03-18 17:47:12 +0700

    decorator_system -> crossmodule and module load/unload outline

--- bot/bot.py
+++ bot/bot.py
@@ -5,35 +5,61 @@ import colorlog
 
 from . import config
 from .rich_guild import guilds, register_bot
+from .crossmodule import CrossModule
 
 MODUBOT_MAJOR = '0'
 MODUBOT_MINOR = '1'
 MODUBOT_REVISION = '0'
 MODUBOT_VERSIONTYPE = 'a'
-MODUBOT_SUBVERSION = '4'
+MODUBOT_SUBVERSION = '5'
 MODUBOT_VERSION = '{}.{}.{}-{}{}'.format(MODUBOT_MAJOR, MODUBOT_MINOR, MODUBOT_REVISION, MODUBOT_VERSIONTYPE, MODUBOT_SUBVERSION)
 MODUBOT_STR = 'ModuBot {}'.format(MODUBOT_VERSION)
 
 class ModuBot(Bot):
     def __init__(self, *args, logname = "ModuBot", conf = config.ConfigDefaults, modulelist = [], loghandlerlist = [], **kwargs):
         self.config = conf
+        self.crossmodule = CrossModule()
         self.log = logging.getLogger(logname)
         for handler in loghandlerlist:
             self.log.addHandler(handler)
         self.log.setLevel(self.config.debug_level)
         super().__init__(command_prefix = self.config.command_prefix, *args, **kwargs)
-        self.remove_command('help')
+        self.help_command = None
 
     def load_module(self, modulename):
+        # 1: walk submodule
+        #     1: submodule pre_init
+        #         this stage should be use to read up config and prepare things such as opening
+        #         port to communicate with server, opening file for logging, register stuff to
+        #         crossmodule object (except features), etc. pre_init must return true if it was
+        #         successful else return false
+        # 2: walk submodule again
+        #     1: load command, group, cogs, ...
+        #         even if some command, group, cogs in a module is not loaded, it will not get skip 
+        #     2: submodule init
+        #         this stage should be use to check commands in the module that got loaded and
+        #         register features available after loaded. init must return true if it was
+        #         successful else return false
+        # 3: walk submodule again
+        #     1: submodule post_init
+        #         this stage should be use to check if dependency loaded correctly with features
+        #         needed. post_init must return true if it was successful else return false
         pass
 
     def load_all_module(self):
         pass
 
     def unload_module(self, modulename):
+        # 1: unload dependents
+        # 2: unload command, group, cogs, ...
+        # 3: remove features
+        # 4: submodule uninit
         pass
 
     def unload_all_module(self):
+        pass
+
+    def register_dependency(self, module, deps_module_list = []):
         pass
 
     async def on_ready(self):
---
+++ bot/crossmodule.py
@@ -0,0 +1,66 @@
+from functools import wraps
+from discord.ext.commands import check as discord_check
+from discord.ext.commands import Context
+
+class CrossModule:
+    def __init__(self):
+        self._decorators = dict()
+        self._preds = dict()
+        self._objs = dict()
+        self._features = dict()
+
+    def register_decorator(self, decorator):
+        self._decorators[decorator.__name__] = decorator
+
+    def unregister_decorator(self, decorator):
+        del self._decorators[decorator.__name__]
+
+    def decorate(self, name, *args, **kwargs):
+        def decorate_use_name(func):
+            @wraps(func)
+            def wrapper(*fargs, **fkwargs):
+                return (self._decorators[name](*args, **kwargs)(func))(*fargs, **fkwargs)
+            return wrapper
+        return decorate_use_name
+
+    def raw_decorator(self, decorator):
+        return self._decorators[decorator.__name__]
+
+    def register_check(self, predicate):
+        self._preds[predicate.__name__] = predicate
+
+    def unregister_check(self, predicate):
+        del self._preds[predicate.__name__]
+
+    def check(self, name):
+        def check_use_name(ctx):
+            return discord_check(self._preds[name](ctx))
+        return check_use_name
+
+    def register_object(self, obj):
+        self._objs[obj.__name__] = obj
+
+    def unregister_object(self, obj):
+        del self._objs[obj.__name__]
+
+    def get_object(self, name):
+        return self._objs[name]
+
+    def _add_module(self, module_name):
+        self._features[module_name] = dict()
+
+    def _remove_module(self, module_name):
+        del self._features[module_name]
+
+    def modules_loaded(self):
+        return self._features.keys().copy()
+
+    def _register_feature(self, module_name, feature, val):
+        self._features[module_name][feature] = val
+
+    def _unregister_feature(self, module_name, feature):
+        del self._features[module_name][feature]
+
+    def feature(self, module_name, feature):
+        # All behaving adults should not modify value returned by this method
+        return self._features[module_name][feature]
